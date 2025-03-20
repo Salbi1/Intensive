7dbdcb70480b3a8fa381b2cf20afc8aa4b85f5ce
salbi <khashi.yashiro@yandex.ru>
2025-03-20 11:41:06 +0300

    first commit

---
+++ src/main/java/com/example/block3/Block3Application.java
@@ -0,0 +1,13 @@
+package com.example.block3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/block3/Block3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.block3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
