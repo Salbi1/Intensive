d74feaaa1fb912766df6541a3386b3693960f655
salbi <khashi.yashiro@yandex.ru>
2025-03-21 09:18:18 +0300

    spring task2 r

--- src/main/java/com/example/block3/filter/CustomHeaderFilter.java
+++ src/main/java/com/example/block3/filter/CustomHeaderFilter.java
@@ -1,14 +1,18 @@
-package com.example.block3.handler;
+package com.example.block3.filter;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import java.io.IOException;
 
+@Component
+@WebFilter("/*")
 public class CustomHeaderFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request,
