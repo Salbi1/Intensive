c86194965fa8844417106ae5814b6735216942c0
salbi <khashi.yashiro@yandex.ru>
2025-03-24 10:04:34 +0300

    dts commit one

---
+++ src/test/java/com/example/DTS/DtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
