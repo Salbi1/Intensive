273df6d11f1e14cba56af1a2de8bb573b6c694be
salbi <khashi.yashiro@yandex.ru>
2025-03-25 09:12:31 +0300

    outBox commit

---
+++ src/main/java/com/example/outBox/OutBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.outBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/outBox/OutBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
